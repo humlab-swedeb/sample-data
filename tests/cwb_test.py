eaabbae275c9289cbc7809f41d6693c97847c8bf
roger-mahler <roger.mahler@hotmail.com>
2023-12-04 13:58:50 +0100

    Debug test

---
+++ tests/cwb_test.py
@@ -0,0 +1,31 @@
+# from ccc import Corpus
+# import pandas as pd
+
+
+# def test_to_cwb():
+#     pass
+
+
+# def test_ccc():
+#     # corpora: Corpora = Corpora(registry_path="/usr/local/share/cwb/registry/")
+#     corpus: Corpus = Corpus(corpus_name="RIKSPROT_V090_TEST", registry_path="/usr/local/share/cwb/registry/")
+
+
+#     subcorpus = corpus.query(
+#         '[lemma="information"]',
+#         context_left=5,
+#         context_right=5,
+#     )
+
+#     data: pd.DataFrame = subcorpus.concordance()
+#     assert data is not None
+
+#     data = subcorpus.collocates()
+#     assert data is not None
+
+#     data = subcorpus.concordance(
+#         p_show=['word', 'pos', 'lemma'],
+#         form='dataframe'
+#     )
+
+#     assert data is not None
